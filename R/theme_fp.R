b2aaa5cac8cacbf4a0b839659b61cee6a137047e
Freddie Partridge <fpartridge@users.noreply.github.com>
2020-04-28 15:58:10 +0100

    Square plot optional now

--- R/theme_fp.R
+++ R/theme_fp.R
@@ -2,7 +2,7 @@
 #'
 #' This function is a theme for ggplot that looks nice to me...
 #' @export
-theme_fp <- function() {
+theme_fp <- function(squarePlot=FALSE) {
   theme_bw(base_size=8) %+replace%
     theme(
       panel.border=element_blank(),
@@ -10,6 +10,9 @@ theme_fp <- function() {
       panel.grid.minor=element_blank(),
       axis.line = element_line(colour = "black", size = 0.5),
       axis.text = element_text(color = "black"),
-      aspect.ratio = 1
+      # aspect.ratio = 1
     )
+  if (squarePlot) {
+    theme(aspect.ratio = 1)
+  }
 }
